7ca2770c2841370f1be46c6db3bcff2ca9d5de5c
m6ttw <matthewrhyswilliams@live.com>
2021-02-25 11:56:38 +0000

    getcourses and getbookings working

--- src/main/java/com/codeclan/example/coursebooking/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/coursebooking/controllers/CourseController.java
@@ -1,4 +1,24 @@
 package com.codeclan.example.coursebooking.controllers;
 
+import com.codeclan.example.coursebooking.models.Course;
+import com.codeclan.example.coursebooking.models.Customer;
+import com.codeclan.example.coursebooking.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/courses")
+    public ResponseEntity<List<Course>> getAllCourses(){
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
 }
