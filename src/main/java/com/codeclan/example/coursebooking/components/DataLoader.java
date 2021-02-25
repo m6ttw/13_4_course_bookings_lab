e559d677a2eaa9754988ac879a7c249be24aac6e
m6ttw <matthewrhyswilliams@live.com>
2021-02-25 11:38:02 +0000

    all tables can populate

--- src/main/java/com/codeclan/example/coursebooking/components/DataLoader.java
+++ src/main/java/com/codeclan/example/coursebooking/components/DataLoader.java
@@ -26,13 +26,26 @@ public class DataLoader implements ApplicationRunner {
     public DataLoader(){}
 
     public void run(ApplicationArguments args){
-        Customer customer = new Customer("Bob", "Edinburgh", 30);
-        customerRepository.save(customer);
+        Customer customer1 = new Customer("Bob", "Edinburgh", 30);
+        customerRepository.save(customer1);
 
-        Course course = new Course("Software Development", "Edinburgh", 4);
-        courseRepository.save(course);
+        Customer customer2 = new Customer("Dave", "Glasgow", 42);
+        customerRepository.save(customer2);
 
-        Booking booking = new Booking("25/02/2021", course, customer);
-        bookingRepository.save(booking);
+        Course course1 = new Course("Software Development", "Edinburgh", 4);
+        courseRepository.save(course1);
+
+        Course course2 = new Course("Hardware Development", "Glasgow", 1);
+        courseRepository.save(course2);
+
+        Booking booking1 = new Booking("25/02/2021", course1, customer1);
+        bookingRepository.save(booking1);
+
+        Booking booking2 = new Booking("12/04/2020", course2, customer2);
+        bookingRepository.save(booking2);
+
+        Booking booking3 = new Booking("23/05/2019", course2, customer1);
+        bookingRepository.save(booking3);
+
     }
 }
