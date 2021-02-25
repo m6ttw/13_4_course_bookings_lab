d7b875ebab720cfe525074884051033e501e9603
m6ttw <matthewrhyswilliams@live.com>
2021-02-25 14:13:46 +0000

    mvp

--- src/main/java/com/codeclan/example/coursebooking/repositories/BookingRepository.java
+++ src/main/java/com/codeclan/example/coursebooking/repositories/BookingRepository.java
@@ -3,5 +3,8 @@ package com.codeclan.example.coursebooking.repositories;
 import com.codeclan.example.coursebooking.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByDate(String date);
 }
